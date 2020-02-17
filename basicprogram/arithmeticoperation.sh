35d042708ca26971f514d0094bcedad2f1be3d61
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-17 15:42:10 +0530

    Refactor : int to float

--- basicprogram/arithmeticoperation.sh
+++ basicprogram/arithmeticoperation.sh
@@ -3,7 +3,7 @@ read -p 'Enter Value : ' b
 read -p 'Enter Value : ' c
 num1=$(( $a + (( $b * $c )) ))
 num2=$(( (( $a % $b )) + $c ))
-num3=$(( $c + (( $a / $b |bc -l )) ))
+num3=`echo $c + $a / $b |bc -l`
 num4=$(( (( $a * $b )) + $c ))
 echo "a+b*c = $num1"
 echo "a%b+c = $num2"
