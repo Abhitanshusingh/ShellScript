8101edbb50db072acf2d71cd8e693885c048b15c
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-18 18:10:34 +0530

    added : Display day of week

---
+++ ifelifandelsestatementprograms/DisplayWeekDay.sh
@@ -0,0 +1,23 @@
+read -p 'Enter number :' day
+if(( $day == 0 ))
+then
+   echo "Sunday"
+elif(( $day == 1 ))
+then
+   echo "Monday"
+elif(( $day == 2 ))
+then
+   echo "Tuesday"
+elif(( $day == 3 ))
+then
+   echo "Wednesday" 
+elif(( $day == 4 ))
+then
+   echo "Thursday"
+elif(( $day == 5 ))
+then
+   echo "Friday"
+elif(( $day  == 6 ))
+then
+   echo "Saturday"
+fi
