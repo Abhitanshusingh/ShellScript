54a7e8f8224b38f5ea3f1a715ff4ba35fc4159d9
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-18 16:31:15 +0530

    added : check day between month

---
+++ ifstatement/CheckDayOfMonth.sh
@@ -0,0 +1,9 @@
+echo $1 $2
+day=$1
+month=$2
+if (( (( $day>= 20 && $month == 03 )) || $month ==04 || $month == 05  || (( $day <= 20 && $month == 06 )) ))
+then 
+echo "True"
+else
+echo "False"
+fi
