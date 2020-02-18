fe25906eeb0c173979c208ea2d7a5be55dac12b6
Abhitanshusingh <60596781+Abhitanshusingh@users.noreply.github.com>
2020-02-18 17:26:40 +0530

    Delete CheckDayOfMonth.sh

--- basicprogram/CheckDayOfMonth.sh
+++
@@ -1,8 +0,0 @@
-read -p 'Enter Day   :' day
-read -p 'Enter Month :' month
-if (( (( $day>= 20 && $month == 03 )) || $month ==04 || $month == 05  || (( $day <= 20 && $month == 06 )) ))
-then 
-echo "True"
-else
-echo "False"
-fi
