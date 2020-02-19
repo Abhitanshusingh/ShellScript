f4b2d0061ed6d97330abe02490ec4c38c5501ad2
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-19 14:51:28 +0530

    Refactor : Indentation

--- case_statement/UnitConversion.sh
+++ case_statement/UnitConversion.sh
@@ -7,19 +7,19 @@ read input
 case "$input" in
    1) feet_to_inch=`echo "scale=2;$number * 12" | bc -l` 
       echo "Feet to inch : $feet_to_inch" 
-   ;;
+      ;;
 
    2) feet_to_meter=`echo "scale=2;$number / 3.281" | bc -l` 
       echo "Feet to meter : $feet_to_meter" 
-   ;;
+      ;;
    
    3) inch_to_feet=`echo "scale=2;$number / 12" | bc -l` 
       echo "Inch to feet : $inch_to_feet" 
-   ;;
+      ;;
   
    4) meter_to_feet=`echo "scale=2;$number * 3.281" | bc -l` 
       echo "Meter to feet : $meter_to_feet"
-   ;;
+      ;;
    
    *) echo "Enter Valid number "
 esac
