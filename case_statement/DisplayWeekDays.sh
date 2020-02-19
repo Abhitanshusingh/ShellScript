3a98e1ce31a69ab32bce862d4515f043ef7e4fdb
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-19 12:05:41 +0530

    added : print day

---
+++ case_statement/DisplayWeekDays.sh
@@ -0,0 +1,18 @@
+read -p "Enter Day : " day  #( 0 Sunday , 1 Monday ,2 Tuesday ...)
+case "$day" in
+     0) echo "Sunday"
+        ;;
+     1) echo "Monday"
+        ;;
+     2) echo "Tuesday"
+        ;;
+     3) echo "Wednesday"
+        ;;
+     4) echo "Thursday"
+        ;;
+     5) echo "Friday"
+        ;;
+     6) echo "Saturday"
+        ;;
+     *) echo " Invalid number enter between 0 to 6"
+esac
