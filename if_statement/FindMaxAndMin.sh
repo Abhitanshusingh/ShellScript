ed0ad4e241fd24f6479ca49c1c5e10c183091f1f
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-19 11:13:55 +0530

    added : generating five random value and fin max and min

---
+++ if_statement/FindMaxAndMin.sh
@@ -0,0 +1,42 @@
+random1=$((RANDOM%900+100))
+random2=$((RANDOM%900+100))
+random3=$((RANDOM%900+100))
+random4=$((RANDOM%900+100))
+random5=$((RANDOM%900+100))
+echo "Random value Generated "
+echo "$random1"
+echo "$random2"
+echo "$random3"
+echo "$random4"
+echo "$random5"
+
+max=$random1
+if (( "$max < $random2" ))
+then
+   max=$random2
+fi
+if (( "$max < $random3" ))
+then
+   max=$random3
+fi
+if (( "$max < $random4" ))
+then
+   max=$random4
+fi
+echo "Maximum value is : $max"
+
+min=$random1
+if (( "$min > $random2" ))
+then
+   min=$random2
+fi
+if (( "$min > $random3" ))
+then
+   min=$random3
+fi
+if (( "$min > $random4" ))
+then
+   min=$random4
+fi
+echo "Minimum value is : $min"
+
