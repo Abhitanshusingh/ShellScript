627599e4af2b2e1f219e8e27ede5ea8a5dba83b1
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-19 10:04:00 +0530

    added

---
+++ if_elif_statement/DecimalValues.sh
@@ -0,0 +1,58 @@
+read -p 'Enter Number :' digit
+if(( $digit > 999 && $digit < 9999 ))     
+then
+   number=$(( $digit % 10 )) 
+   echo "$number : unit"
+   digit=$(( digit/10 ))
+
+   number=$(( $digit % 10 ))
+   echo "$number : ten"
+   digit=$(( digit/10 ))
+ 
+   number=$(( $digit%10 ))
+   echo "$number : hundred"
+   digit=$(( digit/10 ))
+
+
+   number=$(( $digit%10 ))
+   echo "$number : thousand"
+   digit=$(( digit/10 ))
+fi
+
+
+if(( $digit > 99 && $digit < 999 ))    
+then
+   number=$(( $digit % 10 )) 
+   echo "$number : unit"
+   digit=$(( digit/10 ))
+
+   number=$(( $digit % 10 ))
+   echo "$number : ten"
+   digit=$(( digit/10 ))
+
+
+   number=$(( $digit%10 ))
+   echo "$number : hundred"
+   digit=$(( digit/10 ))
+fi
+
+
+if(( $digit > 9 && $digit < 99 ))  
+then
+   number=$(( $digit % 10 )) 
+   echo "$number : unit"
+   digit=$(( digit/10 ))
+
+   number=$(( $digit % 10 ))
+   echo "$number : ten"
+   digit=$(( digit/10 ))
+fi
+
+
+if(( $digit > 0 && $digit < 9 ))   #for 10
+then
+   number=$(( $digit % 10 )) 
+   echo "$number : unit"
+   digit=$(( digit/10 ))
+fi
+
