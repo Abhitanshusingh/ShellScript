61ed69bdc405cbf5f30e2fb0bb8a8800ed3ea260
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-19 10:21:54 +0530

    Refactor : Changed in operatinon

--- basicprogram/ArithmeticOperation.sh
+++ basicprogram/ArithmeticOperation.sh
@@ -1,11 +1,14 @@
 read -p 'Enter Value : ' a
 read -p 'Enter Value : ' b
 read -p 'Enter Value : ' c
-num1=$(( $a + (( $b * $c )) ))
-num2=$(( (( $a % $b )) + $c ))
-num3=`echo $c + $a / $b |bc -l`
-num4=$(( (( $a * $b )) + $c ))
+num1=`echo "scale=2;$a + $b * $c" |bc -l`
+num2=`echo "scale=2;$a % $b + $c" |bc -l`
+num3=`echo "scale=2;$c + $a / $b" |bc -l`
+num4=`echo "scale=2;$a * $b + $c" |bc -l`
 echo "a+b*c = $num1"
 echo "a%b+c = $num2"
 echo "c+a/b = $num3"
 echo "a*b+c = $num4"
+
+
+
