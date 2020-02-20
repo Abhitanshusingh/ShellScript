519d3d8acd938dcbac4d10d9c9527e3a712a7444
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-20 19:00:47 +0530

    Refactor : ArithmeticOperationAndFindMaxMinValue.sh : added debugging mode

--- if_elif_statement/ArithmeticOperationAndFindMaxMinValue.sh
+++ if_elif_statement/ArithmeticOperationAndFindMaxMinValue.sh
@@ -1,43 +1,44 @@
+#!/bin/bash -x
 read -p 'Enter Value : ' a
 read -p 'Enter Value : ' b
 read -p 'Enter Value : ' c
-num1=`echo "scale=2;$a + $b * $c" |bc -l`
-num2=`echo "scale=2;$a % $b + $c" |bc -l`
-num3=`echo "scale=2;$c + $a / $b" |bc -l`
-num4=`echo "scale=2;$a * $b + $c" |bc -l`
-echo "a+b*c = $num1"
-echo "a%b+c = $num2"
-echo "c+a/b = $num3"
-echo "a*b+c = $num4"
+number1=`echo "scale=2;$a + $b * $c" |bc -l`
+number2=`echo "scale=2;$a % $b + $c" |bc -l`
+number3=`echo "scale=2;$c + $a / $b" |bc -l`
+number4=`echo "scale=2;$a * $b + $c" |bc -l`
+echo "a+b*c = $number1"
+echo "a%b+c = $number2"
+echo "c+a/b = $number3"
+echo "a*b+c = $number4"
 
-max=$num1
-if ((`echo "$max < $num2" | bc -q` == 1 ))
+max=$number1
+if ((`echo "$max < $number2" | bc -q` == 1 ))
 then
-   max=$num2
+   max=$number2
 fi
-if ((`echo "$max < $num3" | bc -q` == 1 ))
+if ((`echo "$max < $number3" | bc -q` == 1 ))
 then
-   max=$num3
+   max=$number3
 fi
-if ((`echo "$max < $num4" | bc -q` == 1 ))
+if ((`echo "$max < $number4" | bc -q` == 1 ))
 then
-   max=$num4
+   max=$number4
 fi
 echo "Maximum value is : $max"
 
 
-min=$num1
-if ((`echo "$min > $num2" | bc -q` == 1 ))
+min=$number1
+if ((`echo "$min > $number2" | bc -q` == 1 ))
 then
-   min=$num2
+   min=$number2
 fi
-if ((`echo "$min > $num3" | bc -q` == 1 ))
+if ((`echo "$min > $number3" | bc -q` == 1 ))
 then
-   min=$num3
+   min=$number3
 fi
-if ((`echo "$min > $num4" | bc -q` == 1 ))
+if ((`echo "$min > $number4" | bc -q` == 1 ))
 then
-   min=$num4
+   min=$number4
 fi
 echo "Minimum value is : $min"
 
