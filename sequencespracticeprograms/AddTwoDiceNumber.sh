26f0abe9d080d97cc5074404fd37065892c1f0d8
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-20 18:35:30 +0530

    Refactor : AddTwoDiceNumber.sh : added debugging mode

--- sequencespracticeprograms/AddTwoDiceNumber.sh
+++ sequencespracticeprograms/AddTwoDiceNumber.sh
@@ -1,4 +1,5 @@
-dicefirst=$((RANDOM%6+1))
-dicesecond=$((RANDOM%6+1))
-result=$(( dicefirst + dicesecond ))
+#!/bin/bash -x
+dice_first=$((RANDOM%6+1))
+dice_second=$((RANDOM%6+1))
+result=$(( $dice_first + $dice_second ))
 echo "Sum Of Two Dice Is : $result"
