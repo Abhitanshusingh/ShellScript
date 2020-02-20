67f349b10a6da60a927498b465566527c88f84ab
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-20 19:21:02 +0530

    Refactor : HarmonicNumber.sh  : added debugging mode

--- for_loop_statement/HarmonicNumber.sh
+++ for_loop_statement/HarmonicNumber.sh
@@ -1,9 +1,10 @@
+#!/bin/bash -x
 echo $1
 number=$1
 harmonic_number=0
-for(( i=1 ; i <= $number ; i++ ))
+for(( iter=1 ; iter <= $number ; iter++ ))
 do 
-   harmonic_number=`echo "$harmonic_number + 1 / $i" | bc -l`
+   harmonic_number=`echo "$harmonic_number + 1 / $iter" | bc -l`
 done
 echo "$harmonic_number"
 
