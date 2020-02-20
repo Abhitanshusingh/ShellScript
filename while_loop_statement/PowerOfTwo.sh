4f1b851f6a4b03520bc5bd3a2175dfe010194f61
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-20 19:31:08 +0530

    Refactor : PowerOftwo.sh : added debugging mode

---
+++ while_loop_statement/PowerOfTwo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+number=$1
+power=$(( 2**number ))
+upto=1
+while(( upto <= $power ))
+do 
+   upto=$(( upto*2 ))
+   echo "$upto"
+   if(( $upto==256 ))
+   then
+      break
+   fi 
+done 
