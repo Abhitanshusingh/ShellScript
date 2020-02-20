c1f19304829115d15e19dc704e3583b3ad47ba16
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-20 18:49:58 +0530

    Refactor : CheckDayOfMonth.sh : added debugging mode

--- if_statement/CheckDayOfMonth.sh
+++ if_statement/CheckDayOfMonth.sh
@@ -1,9 +1,9 @@
-echo $1 $2
+#!/bin/bash -x  			
 day=$1
 month=$2
 if (( (( $day>= 20 && $month == 03 )) || $month ==04 || $month == 05  || (( $day <= 20 && $month == 06 )) ))
 then 
-echo "True"
+   echo "True"
 else
-echo "False"
+   echo "False"
 fi
