3e83d54c69b8150bf90dd1f24af3096ab070c927
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-22 09:06:25 +0530

    Refactor: Changed varibale name

--- sequencespracticeprograms/UnitConversion.sh
+++ sequencespracticeprograms/UnitConversion.sh
@@ -2,11 +2,11 @@
 feet=`echo  "scale=2 ; 42/12" | bc -l`
 echo "42 inches is : $feet  feet"
 
-lengthinmeter=`echo "60 * 0.3048" | bc -l` 
-breadthinmeter=`echo "40 * 0.3048" | bc -l`
-totalmeter=`echo "scale =2; $lengthinmeter * $breadthinmeter" | bc -l`
-echo "Rectangular plot of 60 feet * 40 feet is :  $totalmeter"
+length_in_meter=`echo "60 * 0.3048" | bc -l` 
+breadth_in_meter=`echo "40 * 0.3048" | bc -l`
+total_meter=`echo "scale =2; $length_in_meter * $breadth_in_meter" | bc -l`
+echo "Rectangular plot of 60 feet * 40 feet is :  $total_meter"
  
-acre=`echo "scale=2; $totalmeter * 25 / 4046.86 " | bc -l`
+acre=`echo "scale=2; $total_meter * 25 / 4046.86 " | bc -l`
 echo "Areaa of 25 such plot in acer is : $acre"
 
