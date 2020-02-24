b4068f08f337aa4699f576e502e275a6d240a0a8
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-24 14:57:09 +0530

    MODIFIED: Added message and comments

--- array_programs/SumOfThreeInteger.sh
+++ array_programs/SumOfThreeInteger.sh
@@ -1,19 +1,21 @@
 #!/bin/bash -x
 read -p 'Enter size of array: ' length
-echo "Enter $length length"
+echo "Enter  $length  element"
+#Read array from user
 flag=0
 for(( i=0 ;  i < $length ; i++ ))
 do
        read read_array
        array[$i]=$read_array
 done
+
 for(( i=0 ; i < $length-2 ; i++ ))
 do
 	for(( j=$i+1; j < $length-1 ; j++ ))
 	do
 		for(( k=$j+1; k< $length ; k++ ))
 		do
-			if(( `echo "${array[i]}+${array[j]}+${array[k]} ==0 "| bc -q` == 1  ))
+			if(( `echo "${array[i]}+${array[j]}+${array[k]} ==0 "| bc -q` == 1  )) #bc -q  condition true then it return 1 and false when 0 
 			then
 				echo "Triplet is : ${array[i]}  ${array[j]}  ${array[k]} "
 				flag=1
