67d5e401fe84d4642dc7ab2f0ec2d76f3c9eb796
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-24 11:23:43 +0530

    MODIFIED: Changed logic

--- if_statement/LeapYear.sh
+++ if_statement/LeapYear.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -x
-read -p 'Enter Year : ' year
-if (( year <= 999 ))
+read -p 'Enter year : ' year
+if(( ${#year}==4 ))      #Take only four digit number
 then
- echo "Enter four digit number "
-else 
-   if (( (( "$year" % 400 == 0 )) || (( (( "$year" % 4 == 0 )) && (( "$year" % 100 != 0 )) )) )) 
-      then
-         echo "$year : is Leap Year "
-     else
-	 echo "$year : Not Leap Year "
-fi
+	if(( (( "$year" % 400 == 0 )) || (( (( "$year" % 4 == 0 )) && (( "$year" % 100 != 0 )) )) )) 	#Logic for leap year 
+    	then
+      	echo "$year : is Leap Year "
+      else
+			echo "$year : Not Leap Year "
+	fi
+else
+	echo "Enter four digit number"
 fi
