c68e3150dc9817757214288515b9a241796820e4
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-24 10:33:22 +0530

    MODIFIED: Added comments

--- if_statement/CheckDayOfMonth.sh
+++ if_statement/CheckDayOfMonth.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x  			
-day=$1
+day=$1			#Taking command-line argument
 month=$2
+#Checking date and month is between 20-03 to 20-06 
 if (( (( $day>= 20 && $month == 03 )) || $month ==04 || $month == 05  || (( $day <= 20 && $month == 06 )) ))
 then 
    echo "True"
