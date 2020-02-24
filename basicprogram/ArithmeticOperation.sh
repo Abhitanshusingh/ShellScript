a61be6fe754e966d2a5f2e2c1dc08ef26ef99ff7
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-24 10:27:34 +0530

    MODIFIED : Changed varibale name

--- basicprogram/ArithmeticOperation.sh
+++ basicprogram/ArithmeticOperation.sh
@@ -2,14 +2,14 @@
 read -p 'Enter Value : ' a                    #Taking three input
 read -p 'Enter Value : ' b
 read -p 'Enter Value : ' c
-num1=`echo "scale=2;$a + $b * $c" |bc -l`     #scale=2 is used to print 2 digit fractional value
-num2=`echo "scale=2;$a % $b + $c" |bc -l`
-num3=`echo "scale=2;$c + $a / $b" |bc -l`
-num4=`echo "scale=2;$a * $b + $c" |bc -l`
-echo "a+b*c = $num1"
-echo "a%b+c = $num2"
-echo "c+a/b = $num3"
-echo "a*b+c = $num4"
+number1=`echo "scale=2;$a + $b * $c" |bc -l`     #scale=2 is used to print 2 digit fractional value
+number2=`echo "scale=2;$a % $b + $c" |bc -l`
+number3=`echo "scale=2;$c + $a / $b" |bc -l`
+number4=`echo "scale=2;$a * $b + $c" |bc -l`
+echo "a+b*c = $number1"
+echo "a%b+c = $number2"
+echo "c+a/b = $number3"
+echo "a*b+c = $number4"
 
 
 
